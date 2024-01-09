aaf9dae20e489c016a29e8c779f92b3cc2b699bd
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-01-09 18:03:05 +0530

    create home, about, store pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Container } from 'react-bootstrap'
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Store from './pages/Store'
+import About from './pages/About'
 
 function App() {
 
   return (
-    <Container>
+    <Container className='mb-4'>
       <Routes>
-        <Route path='/' element={<Home/>}
+        <Route path='/' element={<Home/>} />
+        <Route path='/store' element={<Store/>} />
+        <Route path='/about' element={<About/>} />
       </Routes>
     </Container>
   )
