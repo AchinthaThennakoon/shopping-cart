d1173e38d190275162adb985a2eaa18df1449502
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-01-11 15:30:53 +0530

    create store item component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,19 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import StoreItems from "../components/StoreItems";
+
 const Store = () => {
   return (
-    <div>Store</div>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item=>(
+          <Col>
+            <StoreItems {...item}/>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
