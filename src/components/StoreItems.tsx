b559ee7e66e824a3d2f9f976b1c4404ab937d762
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-01-11 17:38:21 +0530

    add buttons to change cart items

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import formatCurrency from "../utilities/formatCurrency";
 
 type StoreItemsProps = {
@@ -9,6 +9,7 @@ type StoreItemsProps = {
 };
 
 const StoreItems = ({ id, name, price, imgUrl }: StoreItemsProps) => {
+  const quantity = 0;
   return (
     <>
       <Card>
@@ -19,12 +20,32 @@ const StoreItems = ({ id, name, price, imgUrl }: StoreItemsProps) => {
           style={{ objectFit: "cover" }}
         />
         <Card.Body className="d-flex flex-column">
-            <Card.Title 
-              className="d-flex justify-content-between align-items-baseline mb-4"
-            >
-              <span className="fs-2">{name}</span>
-              <span className="ms-2 text-muted">{formatCurrency(price)}</span>
-            </Card.Title>
+          <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+            <span className="fs-2">{name}</span>
+            <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+          </Card.Title>
+          <div className="mt-auto">
+            {quantity === 0 ? (
+              <Button className="w-100">+ Add to cart</Button>
+            ) : (
+              <div
+                className="d-flex align-items-center flex-column"
+                style={{ gap: ".5rem" }}
+              >
+                <div
+                  className="d-flex align-items-center justify-content-center"
+                  style={{ gap: ".5rem" }}
+                >
+                  <Button>-</Button>
+                  <div>
+                    <span className="fs-3">{quantity}</span> in cart
+                  </div>
+                  <Button>+</Button>
+                </div>
+                <Button variant="danger" size="sm">Remove</Button>
+              </div>
+            )}
+          </div>
         </Card.Body>
       </Card>
     </>
