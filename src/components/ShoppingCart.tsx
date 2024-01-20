3a8a1b075be84c19decccc450ed073adfe9cb213
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-01-20 08:28:47 +0530

    create shooping cart bar

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Offcanvas, Stack } from "react-bootstrap";
+import { useShoppingcart } from "../context/ShoppingCartContext";
+import CartItem from "./CartItem";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart, cartItems } = useShoppingcart();
+  return (
+    <>
+      <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>cart</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Stack gap={3}>
+            {cartItems.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+          </Stack>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
+
+export default ShoppingCart;
