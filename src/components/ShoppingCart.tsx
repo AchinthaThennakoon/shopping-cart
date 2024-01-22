3f6a1f3d4e295a245fddea689d8647379366d6a6
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-01-23 01:05:09 +0530

    add cart prices

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingcart } from "../context/ShoppingCartContext";
 import CartItem from "./CartItem";
+import formatCurrency from "../utilities/formatCurrency";
+import storeItems from "../data/items.json";
+import { JSX } from "react/jsx-runtime";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -11,18 +14,27 @@ const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
   const { closeCart, cartItems } = useShoppingcart();
   return (
     <>
-      <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>cart</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Stack gap={3}>
-            {cartItems.map((item) => (
-              <CartItem key={item.id} {...item} />
-            ))}
-          </Stack>
-        </Offcanvas.Body>
-      </Offcanvas>
+      <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item: JSX.IntrinsicAttributes & { id: number; quantity: number; }) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total: number, cartItem: { id: number; quantity: number; }) => {
+                const item = storeItems.find(i => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </Offcanvas.Body>
+    </Offcanvas>
     </>
   );
 };
